c15cc6bfee27bc9d5dd5df1a984e8e2ae5f43c75
Antoniogtzv <125924468+Antoniogtzv@users.noreply.github.com>
2023-02-20 18:57:49 -0600

    8gu9b`

---
+++ objetos.py
@@ -0,0 +1,14 @@
+#importar la clase 
+from personaje import*
+
+#2.Instanciar un objeto
+Heroe= Personaje()
+
+
+#3. Acceder a sus atributos
+
+print("el personaje pertenece a la  raza : "+ Heroe.especie)
+print("se llama : "+ Heroe.nombre)
+print("El Mide : "+ Heroe.altura + "metros")
+
+print("metodos personaje")
--- personaje.py
+++ personaje.py
@@ -1,7 +1,24 @@
 class Personaje:
     #atributos
     especie= "Humano"
-    nombre= "mARCUS FENIX"
+    nombre= "MARCUS FENIX"
     altura= 19.90
-    
-    
+
+    #Metodos Personaje
+
+    def correr(self,status):
+        if(status):
+            print("el personaje " +self.nombre +"esta corriendo")
+        else:
+            (status)
+            print("el personaje " +self.nombre +"se detubo")
+
+        def lanzargranada(self):
+            print("se lanzo granada")
+
+        def recargaArma(self, municiones):
+            cargador= 5
+            cargador = cargador + municiones
+            print("el arma tiene ahora"+ cargador + "balas")
+
+
