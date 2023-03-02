6b6ff9196f314fcbbc45d9f45102157104a24ff1
Antoniogtzv <125924468+Antoniogtzv@users.noreply.github.com>
2023-03-01 18:19:17 -0600

    agregamos el boton agregar boton

--- Tkinter/practica11.py
+++ Tkinter/practica11.py
@@ -1,5 +1,18 @@
 
-from tkinter import Tk,Frame,Button
+from tkinter import Tk,Frame,Button,messagebox
+
+def mostrarMensaje():
+    messagebox.showinfo("aviso","este mensaje es para avisar algo")
+    messagebox.showerror("Todo Fallo con exito")
+    print(messagebox.askokcancel("pregunta","el o ella jugo con tu corazon"))
+    messagebox.askokcancel("ella jugo con tu corazon")
+
+#agregar botones
+def agregarBoton():
+    botonzul.config(text="+",bg="green",fg="white")
+    botonNuevo=Button(seccion3,text="Boton nuevo")
+    botonNuevo.pack()
+
 
 #1.Creacion de ventana objeto
 ventana= Tk()
@@ -20,17 +33,17 @@ seccion4=Frame(ventana, bg="#0066ff")
 seccion4.pack(expand=True,fill='both')
 
 #botones
-botonzul= Button(seccion1,text="boton azul",fg="blue")
+botonzul= Button(seccion1,text="boton azul",fg="blue",command=mostrarMensaje)
 botonzul.place(x=60, y=60)
 
-BotonNegro= Button(seccion2,text="boton negro",fg="#ff6699")
+BotonNegro= Button(seccion2,text="boton negro",fg="#000000",command=agregarBoton)
 BotonNegro.grid(row=0, column=1)
 
-BotonNegro= Button(seccion2,text="boton verde",fg="green")
-BotonNegro.grid(row=0, column=2)
+BotonVerde= Button(seccion2,text="boton verde",fg="green")
+BotonVerde.grid(row=0, column=2)
 
-BotonNegro= Button(seccion3,text="boton verde",fg="green",bg="#ff6699")
-BotonNegro.grid(row=1, column=1)
+BotonRojo= Button(seccion3,text="boton rojo",fg="red",bg="#A52A2A")
+BotonRojo.grid(row=1, column=1)
 
 
 
